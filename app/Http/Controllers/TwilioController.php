95c930aef8a4e7a9b75415b3ad28ef7cd1b44a93
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2022-08-13 04:35:01 +0300

    fix twilio service

---
+++ app/Http/Controllers/TwilioController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TwilioController extends Controller
+{
+    public function sendMessage()
+    {
+        $ssid = "AC7cb2ba291d3fcdccc4a52934b3cc2f7c";
+        $token = "fe9a4b4772aed0e5571c53b1f1c5de52";
+        $twiloClient = new \Twilio\Rest\Client($ssid, $token);
+        $twiloClient->messages->create(
+            '+261348974586',
+            ['from' => '+12242617812', 'body' => rand(10000, 999999)]
+        );
+    }
+}
