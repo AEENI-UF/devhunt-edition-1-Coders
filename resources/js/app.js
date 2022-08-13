c3926a2e2046cab46c09deeefb7aec44004fc2d5
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2022-08-13 05:26:31 +0300

    feat footer component

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,6 +30,10 @@ Vue.component(
     require("./components/client/menu/MenuComponent.vue").default
 );
 
+Vue.component(
+    "footer-component",
+    require("./components/client/footer/FooterComponent.vue").default
+);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
