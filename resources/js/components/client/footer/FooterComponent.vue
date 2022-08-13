6e57a824b9e7970e1c086be4db746f84e9aa5ee2
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2022-08-13 16:45:31 +0300

    feat lien for routes

--- resources/js/components/client/footer/FooterComponent.vue
+++ resources/js/components/client/footer/FooterComponent.vue
@@ -1,32 +1,12 @@
 <template>
     <footer id="footer">
-        <div class="footer-newsletter">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-6">
-                        <h4>Joindre votre lettre d'information </h4>
-                        <p>
-                            Ici, vous pouvez joindre ou même rattacher votre lettre d'information
-                            pour y renseigner .
-                        </p>
-                        <form action="" method="post">
-                            <input type="email" name="email" /><input
-                                type="submit"
-                                value="Souscrire"
-                            />
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <div class="footer-top">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-3 col-md-6 footer-contact">
                         <h3>ENI</h3>
                         <p>
-                            Antaninarenina Tanambao BP 1487  <br />
+                            Antaninarenina Tanambao BP 1487 <br />
                             FIANARANTSOA,301 <br />
                             MADAGASCAR <br /><br />
                             <strong>Tél:</strong> 034 05 733 36<br />
@@ -57,25 +37,26 @@
                         <ul>
                             <li>
                                 <i class="bx bx-chevron-right"></i>
-                                <a href="#">Génie Logiciel et base de données</a>
+                                <a href="#"
+                                    >Génie Logiciel et base de données</a
+                                >
                             </li>
                             <li>
                                 <i class="bx bx-chevron-right"></i>
-                                <a href="#">Administration et Systèmes Réseaux</a>
+                                <a href="#"
+                                    >Administration et Systèmes Réseaux</a
+                                >
                             </li>
                             <li>
                                 <i class="bx bx-chevron-right"></i>
                                 <a href="#">Informatique Générale</a>
                             </li>
-                           
                         </ul>
                     </div>
 
                     <div class="col-lg-3 col-md-6 footer-links">
                         <h4>Notre réseaux sociaux</h4>
-                        <p>
-                            Suivez-nous sur les réseaux sociaux suivants.
-                        </p>
+                        <p>Suivez-nous sur les réseaux sociaux suivants.</p>
                         <div class="social-links mt-3">
                             <a href="#" class="twitter"
                                 ><i class="bx bxl-twitter"></i
@@ -98,7 +79,7 @@
             </div>
         </div>
         <div class="container footer-bottom clearfix">
- <div class="copyright">
+            <div class="copyright">
                 &copy; Copyright <strong><span>ENI</span></strong
                 >. Tout Droit réservé
             </div>
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::group('/authentification', function () {
+Route::group(['/authentification'], function () {
     Route::get('/etudiant', function () {
         return view('auth.etudiant-login');
     });
