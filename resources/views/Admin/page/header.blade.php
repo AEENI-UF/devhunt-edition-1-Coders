da2d9bd64964f26bfbf5370f77125fe8ad8dd74c
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2022-08-14 22:53:08 +0300

    fix dashboard

--- resources/views/Admin/page/header.blade.php
+++ resources/views/Admin/page/header.blade.php
@@ -152,7 +152,7 @@
             <div class="page-breadcrumb">
                 <div class="row">
                     <div class="col-12 d-flex no-block align-items-center">
-                        <h4 class="page-title">Accueil</h4>
+                        <h4 class="page-title">DASHBOARD</h4>
                         <div class="ml-auto text-right">
                         </div>
                     </div>
