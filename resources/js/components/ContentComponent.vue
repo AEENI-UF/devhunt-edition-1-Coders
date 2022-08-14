9ef0f577043ce0a61dff6b493ad58631a17987b6
Mahitsilaza12 <mahitsilazafabrila@gmail.com>
2022-08-14 09:08:55 +0200

    mise en place du logo du partenaire

--- resources/js/components/ContentComponent.vue
+++ resources/js/components/ContentComponent.vue
@@ -18,7 +18,7 @@
                         class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center"
                     >
                         <img
-                            src="assets/img/clients/client-2.png"
+                            src="arato.JPG"
                             class="img-fluid"
                             alt=""
                         />
@@ -28,7 +28,7 @@
                         class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center"
                     >
                         <img
-                            src="assets/img/clients/client-3.png"
+                            src="log1.JPG"
                             class="img-fluid"
                             alt=""
                         />
@@ -38,7 +38,7 @@
                         class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center"
                     >
                         <img
-                            src="assets/img/clients/client-4.png"
+                            src="numeri.png"
                             class="img-fluid"
                             alt=""
                         />
@@ -48,7 +48,7 @@
                         class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center"
                     >
                         <img
-                            src="assets/img/clients/client-5.png"
+                            src="vilia.png"
                             class="img-fluid"
                             alt=""
                         />
@@ -58,7 +58,7 @@
                         class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center"
                     >
                         <img
-                            src="assets/img/clients/client-6.png"
+                            src="WeCours.JPG"
                             class="img-fluid"
                             alt=""
                         />
